cef8a95d8709ad0a7d31cecf9df0a2c21c0ef8e9
emmas97 <78239177+emmas97@users.noreply.github.com>
2021-05-10 12:03:46 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace JavaValidationCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			< !DOCTYPE html >
+ < html >
+ < body >
+ 
+
+
+ < h1 > Customer Information </ h1 >
+	< form action = "/action_page.php" >
+	 
+		 < label for= "fname" > First name:</ label >
+	  
+		  < input type = "text" id = "fname" name = "fname" >< br >< br >
+	  
+		  < label for= "lname" > Last name:</ label >
+	   
+		   < input type = "text" id = "lname" name = "lname" >< br >< br >
+	   
+		   < label for= "email" > E - mail:</ label >
+		
+			< input type = "text" id = "email" name = "email" >< br >< br >
+		
+
+		< button type = "button" > Submit </ button >
+		
+
+
+
+		</ form >
+		
+
+		< script >
+
+		function validateForm() {
+						var x = document.forms["myForm']["email"].value;
+			if (x == "")
+						{
+							alert("E-Mail must be filled out.");
+							return false;
+						}
+					}
+		}
+    }
+}
